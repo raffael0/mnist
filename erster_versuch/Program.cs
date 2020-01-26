8df409ab5e9fb264b9a52812eb7cfa1582db8ec3
raffael <raffaelrott@protonmail.com>
2020-01-26 14:40:41 +0100

    Initial commit, no back-propagation, Parser done

---
+++ erster_versuch/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace erster_versuch
+{
+    class Program
+    {
+        
+        
+        static void Main(string[] args)
+        {
+        Parser parser = new Parser();
+        parser.Parse("training_data/train-images-idx3-ubyte", "training_data/train-labels-idx1-ubyte");
+        Network network = new Network(28*28,10,2,40);
+        network.cycle(parser.Images);
+        }
+       
+    }
+}
